b2bc916e296ec27b9ab8027669778bd0f1839893
Ali Imran Alić <aliimranalic6@gmail.com>
2025-01-11 20:33:16 +0100

    fixing

--- src/test/java/org/example/TestTickets.java
+++ src/test/java/org/example/TestTickets.java
@@ -1,4 +1,50 @@
 package org.example;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestTickets {
+    private static WebDriver webDriver;
+    private static String baseUrl;
+
+    @BeforeAll
+    public static void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Korisnik\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"); // specify the path to chromedriver
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        webDriver = new ChromeDriver(options);
+        baseUrl = "https://www.premierleague.com/";
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        webDriver.manage().window().maximize();
+
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        if (webDriver != null) {
+            webDriver.quit();
+        }
+    }
+
+    @Test
+    public void testAutoSuggestion () throws InterruptedException {
+
+    }
+
 }
+
+
+
