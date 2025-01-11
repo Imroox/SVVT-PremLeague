3b864d5049c04a8fb29b8efb6e4403ad18580908
Ali Imran Alić <aliimranalic6@gmail.com>
2025-01-12 00:13:25 +0100

    added test fix

--- src/test/java/org/example/TestCheckPlayerStats.java
+++ src/test/java/org/example/TestCheckPlayerStats.java
@@ -1,4 +1,78 @@
 package org.example;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestReporter;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+import java.util.Random;
+
+import static java.nio.file.Paths.get;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestCheckPlayerStats {
+    private static WebDriver webDriver;
+    private static String baseUrl;
+
+    @BeforeAll
+    public static void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Korisnik\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"); // specify the path to chromedriver
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        webDriver = new ChromeDriver(options);
+        baseUrl = "https://www.premierleague.com/";
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        webDriver.manage().window().maximize();
+
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        if (webDriver != null) {
+            webDriver.quit();
+        }
+    }
+    @Test
+    public void checkStatsOfPlayer() throws InterruptedException {
+        webDriver.get("https://www.premierleague.com/");
+        Thread.sleep(1000);
+        webDriver.findElement(By.xpath("//*[@id=\"onetrust-accept-btn-handler\"]")).click();
+        Thread.sleep(3000);
+        webDriver.findElement(By.xpath("/html/body/header/div/nav/ul/li[13]/a")).click();
+        Thread.sleep(3000);
+        webDriver.findElement(By.xpath("//*[@id=\"search-input\"]")).sendKeys("Jamie Vardy");
+        Thread.sleep(2000);
+        webDriver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[2]/header/div/div/div/div")).click();
+        Thread.sleep(3000);
+        webDriver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[2]/div/div/div/table/tbody/tr/td[1]/a")).click();
+        Thread.sleep(2000);
+        webDriver.findElement(By.cssSelector("#mainContent > div.wrapper.hasFixedSidebar > div > div > nav > ul > li:nth-child(2) > a")).click();
+        Thread.sleep(10000);
+
+        WebElement totalGoalsStat = webDriver.findElement(By.cssSelector("#mainContent > div.wrapper.hasFixedSidebar > div > div > div > div > div > div:nth-child(2) > span > span"));
+        String actualTotalGoals = totalGoalsStat.getText();
+        String expectedTotalGoals = "142";
+        assertEquals(expectedTotalGoals, actualTotalGoals);
+
+
+        WebElement totalAssistStat = webDriver.findElement(By.cssSelector("#mainContent > div.wrapper.hasFixedSidebar > div > div > div > div > ul > li:nth-child(2) > div > div:nth-child(2) > span"));
+        String actualAssistStat = totalAssistStat.getText();
+        String expectedAssistStat = "47";
+        assertEquals(expectedAssistStat, actualAssistStat);
+
+
+    }
+
+
+
+
+
+
+
 }
---
+++ src/test/java/org/example/TestManager.java
@@ -0,0 +1,87 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestReporter;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+import java.util.Random;
+
+import static java.nio.file.Paths.get;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestManager {
+    private static WebDriver webDriver;
+    private static String baseUrl;
+
+    @BeforeAll
+    public static void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Korisnik\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"); // specify the path to chromedriver
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        webDriver = new ChromeDriver(options);
+        baseUrl = "https://www.premierleague.com/";
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        webDriver.manage().window().maximize();
+
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        if (webDriver != null) {
+            webDriver.quit();
+        }
+    }
+
+    @Test
+    public void checkManager() throws InterruptedException {
+        webDriver.get(baseUrl);
+        JavascriptExecutor js = (JavascriptExecutor) webDriver;
+
+        Thread.sleep(4000);
+        webDriver.findElement(By.xpath("//*[@id=\"onetrust-accept-btn-handler\"]")).click();
+        webDriver.findElement(By.xpath("//*[@id=\"advertClose\"]")).click();
+
+        WebElement hoverElement = webDriver.findElement(By.xpath("//*[@id=\"mainNav\"]/nav/ul/li[6]/div[1]"));
+        Actions actions = new Actions(webDriver);
+        actions.moveToElement(hoverElement).perform();
+        Thread.sleep(5000);
+        webDriver.findElement(By.xpath("//*[@id=\"mainNav\"]/nav/ul/li[6]/div[2]/ul/li[4]/a")).click();
+        webDriver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[2]/div/div[1]/section/div[2]/div[2]")).click();
+        Thread.sleep(3000);
+        js.executeScript("arguments[0].click();",webDriver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[2]/div/div[1]/section/div[2]/div[2]")));
+
+
+        webDriver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[2]/div/div[1]/section/div[2]/div[3]/ul/li[3]")).click();
+        Thread.sleep(4000);
+        webDriver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[2]/div/div[1]/div/table/tbody/tr/td[1]/a")).click();
+        Thread.sleep(4000);
+
+        int winStat = Integer.parseInt(webDriver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[2]/nav/div/section[2]/div/div[2]/div[2]")).getText());
+        assertEquals(winStat, 69);
+
+        int drawStat = Integer.parseInt(webDriver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[2]/nav/div/section[2]/div/div[3]/div[2]")).getText());
+        assertEquals(drawStat, 30);
+
+        int loseStat = Integer.parseInt(webDriver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[2]/nav/div/section[2]/div/div[4]/div[2]")).getText());
+        assertEquals(loseStat, 35);
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
